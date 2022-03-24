1c8856ba58389ca8b46a6556af9e04c6ea03d926
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-03-24 21:32:14 +0400

    redid my screen function

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-//check to see if we have our script loaded and ready to go
-const btn = document.getElementById('reset');
+// //Fix screens display and show numbers when they're clicked 
+// const clickNumber = document.getElementById('number');
 
-btn.addEventListener('click', function onClick(event) {
-  // 👇️ change background color
-    document.body.style.backgroundColor = 'salmon';
+// clickNumber.addEventListener('click', function onClick(event) {
+//   // 👇️ change background color
+//     document.body.style.backgroundColor = 'salmon';
 
-  // 👇️ optionally change text color
-  // document.body.style.color = 'white';
+//   // 👇️ optionally change text color
+//   // document.body.style.color = 'white';
+// });
+
+
+//Fix screens display and show numbers when they're clicked 
+document.querySelectorAll('button').forEach(el =>{
+  el.addEventListener('click', () =>{
+    digit_pressed(el.textContent);
+  });
 });
+function digit_pressed(digit) {
+  console.log("digit pressed: " + digit);
+}
